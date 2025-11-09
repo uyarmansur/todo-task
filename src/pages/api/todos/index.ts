47b7da28187b37b39999e26788cf8ecc74754a42
Mansur Uyar <ymansuruyar@gmail.com>
2025-11-10 00:42:54 +0300

    try catch yapısı eklendi

--- src/pages/api/todos/index.ts
+++ src/pages/api/todos/index.ts
@@ -6,27 +6,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    const todos = await prisma.todo.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return res.status(200).json({
-      success: true,
-      message: "Todos fetched successfully",
-      data: todos,
-    });
-  }
-  if (req.method === "POST") {
-    const todo = req.body;
-    const newTodo = await prisma.todo.create({
-      data: todo,
-    });
-    return res.status(201).json({
-      success: true,
-      message: "Todo created successfully",
-      data: newTodo,
+  try {
+    if (req.method === "GET") {
+      const todos = await prisma.todo.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      return res.status(200).json({
+        success: true,
+        message: "Todos fetched successfully",
+        data: todos,
+      });
+    }
+    if (req.method === "POST") {
+      const todo = req.body;
+      const newTodo = await prisma.todo.create({
+        data: todo,
+      });
+      return res.status(201).json({
+        success: true,
+        message: "Todo created successfully",
+        data: newTodo,
+      });
+    }
+  } catch (error) {
+    console.error("Error handling /api/todos request:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
 }
